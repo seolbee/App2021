59908edb81051dde480b5aa1ad5c0887af79d796
seolbee <tmf0909@daum.net>
2021-03-08 12:55:38 +0900

    레아이웃 짜기2

---
+++ sm3_8/app/src/main/java/com/example/sm3_8/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.sm3_8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText num1, num2;
+    private Button add, sub, mul, div;
+    private TextView result;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        num1 = findViewById(R.id.num1);
+        num2 = findViewById(R.id.num2);
+
+        add = findViewById(R.id.add);
+        sub = findViewById(R.id.sub);
+        mul = findViewById(R.id.mul);
+        div = findViewById(R.id.div);
+
+        result = findViewById(R.id.txt1);
+
+        add.setOnClickListener(this);
+        sub.setOnClickListener(this);
+        mul.setOnClickListener(this);
+        div.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int result_num = 0;
+        switch(v.getId()){
+            case R.id.add :
+                result_num = Integer.parseInt(num1.getText().toString()) + Integer.parseInt(num2.getText().toString());
+                break;
+            case R.id.sub :
+                result_num = Integer.parseInt(num1.getText().toString()) - Integer.parseInt(num2.getText().toString());
+                break;
+            case R.id.mul :
+                result_num = Integer.parseInt(num1.getText().toString()) * Integer.parseInt(num2.getText().toString());
+                break;
+            case R.id.div :
+                result_num = Integer.parseInt(num1.getText().toString()) / Integer.parseInt(num2.getText().toString());
+                break;
+        }
+
+        result.setText("계산한 결과: " + result_num);
+    }
+}
